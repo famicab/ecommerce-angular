11cde8c706fe19485a0e44704e7bea367bb1c1e6
famicab <cesco.deamicis@gmail.com>
2024-03-25 22:38:42 +0100

    First config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Component, OnInit } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule, MenubarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ecommerce-angular';
+
+  items: MenuItem[] | undefined;
+  ngOnInit(): void {
+    this.items = [
+      {
+        label: 'Home',
+        icon: 'pi pi-fw pi-home'
+      },
+      {
+        label: 'Cart',
+        icon: 'pi pi-fw pi-shopping-cart'
+      }
+    ]
+  }
 }
